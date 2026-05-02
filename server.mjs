0e527494d7a7195a9cd2605ff3ed85031e052b66
Kirill <kirillrubcov990@gmail.com>
2026-05-02 14:30:25 +0300

    Initial commit

---
+++ server.mjs
@@ -0,0 +1,74 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import { MongoClient } from "mongodb";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { v4 as uuidv4 } from "uuid";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+
+const client = new MongoClient("mongodb://localhost:27017");
+const db = client.db("myAppDB");
+const collection = db.collection("users");
+
+async function start() {
+    await client.connect();
+
+    io.on("connection", (socket) => {
+        console.log("CONNECTED:", socket.id);
+    });
+
+    server.listen(3000, () => {
+        console.log("Server running on 3000");
+    });
+}
+
+start();
+
+app.get("/api/users", async (req, res) => {
+
+    const info = req.query
+    res.json(info)
+
+})
+
+app.post("/api/users", async (req, res) => {
+    try {
+
+        const user = {
+            id: uuidv4(),
+            name: req.query.name.trim(),
+            surename: req.query.surename.trim()
+        };
+
+        if (!user.name || !user.surename) {
+            return res.status(400).json("Missing name or surname");
+        }
+
+        const result = await collection.insertOne(user);
+
+        return res.status(201).json(result);
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err.message);
+    }
+});
+
+app.delete("/api/users", async (req, res) => {
+
+    const { id } = req.query
+    const result = await collection.deleteOne({ id: req.query.id });
+
+    res.json(result)
+});
+
+//node server.mjs
